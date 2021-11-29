a99cf9074b9d1d77c12390e2accf9a18be67011e
johnvwood <j_wood112035@fanshaweonline.ca>
2021-11-28 23:35:21 -0500

    fixed everything

--- includes/mail.php
+++ includes/mail.php
@@ -6,62 +6,80 @@ header('Content-Type: application/json; charset=UTF-8');
 $results = [];
 $visitor_name = '';
 $visitor_email = '';
-$visitor_message = '';
+$visitor_comment = '';
 $visitor_topic = '';
 
 
 // Validate
-if (isset($_POST['firstname']) && $_POST['firstname'] !='') {
+if (isset($_POST['firstname'])&& $_POST['firstname'] !=''){
     $visitor_name = filter_var(htmlspecialchars($_POST['firstname']), FILTER_SANITIZE_STRING);
 } else {echo "First name not valid";}
 
-if (isset($_POST['lastname']) && $_POST['lastname'] !=''){
+if (isset($_POST['lastname'])&& $_POST['lastname'] !=''){
     $visitor_name .= ' '.filter_var(htmlspecialchars($_POST['lastname']), FILTER_SANITIZE_STRING);
 } else {echo "Last name not valid";}
 
-if (isset($_POST['email']) && $_POST['email'] !=''){
+if (isset($_POST['email'])&& $_POST['email'] !=''){
     $visitor_email = filter_var(htmlspecialchars($_POST['email']), FILTER_VALIDATE_EMAIL);
 } else {echo "Email not valid";}
 
-if (isset($_POST['message'])){
-    $visitor_message = filter_var(htmlspecialchars($_POST['message']), FILTER_SANITIZE_STRING);
-}
+if (isset($_POST['comment'])){
+    $visitor_comment = filter_var(htmlspecialchars($_POST['comment']), FILTER_SANITIZE_STRING);
+} else {echo "Please enter valid text";}
 
 
 // Validate Dropdown 
 if (isset($_POST['topic']) && $_POST['topic'] !='') {
-    $visitor_topic = filter_var(htmlspecialchars($_POST['topic']), FILTER_SANITIZE_STRING);
+    $visitor_topic = filter_var(htmlspecialchars($_POST['topic']));
 } else {echo "Topic not selected";}
   
 $results['name'] = $visitor_name;
-$results['message'] = $visitor_message;
+$results['email'] = $visitor_email;
+$results['comment'] = $visitor_comment;
+$results['topic'] = $visitor_topic;
 
 
-// Dropdown Categories, could go to different emails, kept same for testing
-if ($visitor_topic == "project") {
-    $email_recepient = 'woodjoh783@gmail.com';
-}
-else if ($visitor_topic == "feedback") {
-    $email_recepient = 'woodjoh783@gmail.com';
-}
-else if ($visitor_topic == "other") {
-    $email_recepient = 'woodjoh783@gmail.com';
-}
-else { $email_recepient = 'woodjoh783@gmail.com';}
+// require_once('recaptchalib.php');
+//     $privatekey = "your_private_key";
+//     $resp = recaptcha_check_answer (
+//         $privatekey,
+//         $_SERVER["REMOTE_ADDR"],
+//         $_POST["recaptcha_challenge_field"],
+//         $_POST["recaptcha_response_field"]);
+
+//   if (!$resp->is_valid) {
+//     die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+//          "(reCAPTCHA said: " . $resp->error . ")");
+//   }
+
 
 // Email Setup
-$email_subject = sprintF('PHP Form %s', $visitor_topic);
-$email_message = sprintF('Name: %s, Email: %s, Message: %s', $visitor_name, $visitor_email, $visitor_message);  
-$email_headers = array( 'From'=> $visitor_email );
+if ($visitor_topic == "project") {
+    $email_recipient = 'woodjoh783@gmail.com';
+}
+else if ($visitor_topic == "feedback") {
+    $email_recipient = 'woodjoh783@gmail.com';
+}
+else if ($visitor_topic == "other") {
+    $email_recipient = 'woodjoh783@gmail.com';
+}
+else { $email_recipient = 'woodjoh783@gmail.com';}
+
+$email_subject = sprintf('PHP Form %s', $visitor_topic);
+$email_message = sprintf('Name: %s, Email: %s, Comment: %s', $visitor_name, $visitor_email, $visitor_comment);  
+$email_headers = array( 
+    'Reply-To' => $visitor_email,
+    'From'=>$visitor_email 
+);
 
 
 // Send Email
-$email_result = mail($email_recepient, $email_subject, $email_message, $email_headers);
+$email_result = mail($email_recipient, $email_subject, $email_message, $email_headers);
 
 if ($email_result) {
-    $results['message'] = sprintf('Thank you for reaching out to us, %s. You will be contacted shortly.', $visitor_name);
+    $results['message'] = sprintf('Thank you for reaching out to us, %s. You will be contacted within 3 business days.', $visitor_name);
 } else {
-    $results['message'] = sprintf('We are sorry but the email did not go through.');
+    $results['message'] = ('There seems to be an error in sending the email.');
 } 
 
-echo(json_encode($results));
+echo json_encode($results);
--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 import {SendMail} from "./modules/mailer.js";
-
 (() => {
-    let mailSubmit = document.querySelector('.submit-wrapper');
-
+    let mailSubmit = document.querySelector('.submit-box');
     function processMailFailure(result) {
-        console.table(result);
-        console.log(result.message);
+        console.table(result); 
     }
-
     function processMailSuccess(result) {
         console.table(result);
-        alert(result.message);
     }
-
     function processMail(event) {
-        // block the default submit behaviour
         event.preventDefault();
         SendMail(this.parentNode)
             .then(data => processMailSuccess(data))
             .catch(err => processMailFailure(err));
     }
-
     mailSubmit.addEventListener("click", processMail);
 })();
